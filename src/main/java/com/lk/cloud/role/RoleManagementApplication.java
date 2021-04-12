aab99f9131e4136f019bda0637eb29cbc57ae8e0
vcgdev <vcg_cruz@hotmail.com>
2021-04-12 08:11:47 -0500

    add CORS and security configurations

--- src/main/java/com/lk/cloud/role/RoleManagementApplication.java
+++ src/main/java/com/lk/cloud/role/RoleManagementApplication.java
@@ -1,5 +1,7 @@
 package com.lk.cloud.role;
 
+import java.util.Arrays;
+
 import com.lk.cloud.role.config.auth.IAuthenticationFacade;
 import com.lk.cloud.role.config.auth.RestTemplateInterceptor;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RoleManagementApplication {
@@ -26,6 +31,20 @@ public class RoleManagementApplication {
 		restTemplate.getInterceptors()
 			.add(new RestTemplateInterceptor(authenticationFacade));
 		return restTemplate;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(Arrays.asList("http://*", "chrome-extension**", "**"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
